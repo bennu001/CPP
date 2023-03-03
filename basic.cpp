405c3699d0fd7ca5cfd5acf39d053a0dff099767
Bennu Sai <saibennu2001@gmail.com>
2023-03-03 22:51:30 +0530

    first changes

---
+++ basic.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "hello" << endl;
+
+    int x, y;
+    cin >> x >> y;
+    cout << "value of x:" << x << "y is" << y;
+    return 0;
+}
